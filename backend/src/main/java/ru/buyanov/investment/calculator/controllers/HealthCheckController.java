a22366b928494948987fa3c3d3ac7751c60332ee
Alex <dragnir25@mail.ru>
2019-04-14 14:05:16 +0200

    health check app

---
+++ backend/src/main/java/ru/buyanov/investment/calculator/controllers/HealthCheckController.java
@@ -0,0 +1,15 @@
+package ru.buyanov.investment.calculator.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("health")
+public class HealthCheckController {
+
+    @RequestMapping("")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Feeling good today");
+    }
+}
