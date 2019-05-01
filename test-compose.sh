ca68f63fb6fd6504a45d00eee6a18a69e8405178
Alex <dragnir25@mail.ru>
2019-05-01 15:09:24 +0200

    add docker-compose with nginx and test for them

---
+++ test-compose.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+printf "\n127.0.0.1    backend.sample.buyanov.ru" | sudo tee -a /etc/hosts
+docker-compose up -d
+sleep 30
+FIRST_LINE=$(curl -I -X GET http://backend.sample.buyanov.ru:1080/health | head -n 1)
+IS_NOT_200_OK=$(echo ${FIRST_LINE} | grep 200)
+if [ -z "$IS_NOT_200_OK" ]
+then
+    echo "service is not accessible"
+    echo ${FIRST_LINE}
+    echo "exiting"
+    exit 1
+else
+    exit 0
+fi
