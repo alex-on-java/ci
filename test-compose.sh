0b205a65c1dfbbe791b5aa5f62f45b8b67f2fe2e
Alex <dragnir25@mail.ru>
2019-05-09 15:24:05 +0200

    set external network, rename services to avoid name collision

--- test-compose.sh
+++ test-compose.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DOMAIN=${1?Ops. Argument missing. Please provide the full domain that should be checked}
+docker network create lb_main-net
 docker-compose up -d
 sleep 30
 FIRST_LINE=$(curl -I -X GET http://$DOMAIN:1080/health | head -n 1)
